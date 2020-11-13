9ac2db983169345c6ba57ab3d28a7aae31154776
Chris Filipowski <chris.filipowski@canva.com>
2020-11-13 11:22:08 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+FROM gkswjdzz/deoldify-models AS build
+FROM nvcr.io/nvidia/pytorch:19.04-py3
+
+RUN apt-get -y update
+
+RUN apt-get install -y python3-pip software-properties-common wget ffmpeg
+
+RUN apt-get -y update
+
+RUN mkdir -p /root/.torch/models
+
+RUN mkdir -p /data/upload
+
+ADD requirements.txt /data/
+
+WORKDIR /data
+
+RUN pip install -r requirements.txt
+RUN pip install Flask
+RUN pip install Pillow
+RUN pip install scikit-image
+RUN pip install requests
+
+COPY --from=build /models/ /root/.torch/models
+RUN mkdir -p /data/models
+RUN mv /root/.torch/models/ColorizeArtistic_gen.pth /data/models/ColorizeArtistic_gen.pth
+
+ADD . /data/
+EXPOSE 80
+
+CMD ["python3", "app.py"]
---
+++ app.py
@@ -0,0 +1,171 @@
+import os
+import sys
+import requests
+import ssl
+from flask import Flask, redirect, url_for, request
+from flask import request
+from flask import jsonify
+from flask import send_file
+
+
+from app_utils import download, DownloadPrecheckFailed
+from app_utils import generate_random_filename
+from app_utils import clean_me
+from app_utils import clean_all
+from app_utils import create_directory
+from app_utils import get_model_bin
+from app_utils import convertToJPG
+
+from os import path
+import torch
+
+import fastai
+from deoldify.visualize import *
+from pathlib import Path
+import traceback
+
+
+torch.backends.cudnn.benchmark=True
+
+#os.environ['CUDA_VISIBLE_DEVICES']='0'
+
+app = Flask(__name__)
+
+
+@app.route("/canva/editing/image/process", methods=["POST"])
+def process_image_for_canva():
+    input_path = generate_random_filename(upload_directory,"jpeg")  # /data/upload/0232323.jpeg'
+    output_path = os.path.join(results_img_directory, os.path.basename(input_path)) # /data/results_images/0232323.jpeg
+
+    print("Input path:", input_path)
+    print("Output path:", output_path)
+
+    try:
+        # passing a imageUrl in a body payload
+        url = request.json["imageUrl"]
+        render_factor = int(request.json["render_factor"])
+        print("imageUrl", url)
+        # render_factor = 35 #int(request.json["render_factor"])
+
+        download(url, input_path)
+        print("downloaded")
+
+        run(input_path, render_factor)
+        print("converted")
+        
+        callback = send_file(output_path, mimetype='image/jpeg')
+    
+        return callback, 200
+
+    except DownloadPrecheckFailed as e:
+        return jsonify({'message': str(e)}), 400
+    except:
+        traceback.print_exc()
+        return jsonify({'message': 'inference error'}), 500
+
+    finally:
+        pass
+        clean_all([
+            input_path,
+            output_path
+            ])
+
+# define a predict function as an endpoint
+@app.route("/process-img", methods=["POST"])
+def process_image():
+    input_path = generate_random_filename(upload_directory,"jpeg")
+    output_path = os.path.join(results_img_directory, os.path.basename(input_path))
+
+    print("Input path:", input_path)
+    print("Output path:", output_path)
+
+    try:
+        # passing a source_url as a payload
+        url = request.json["source_url"]
+        render_factor = int(request.json["render_factor"]
+        print("source_url", url)
+
+        download(url, input_path)
+        print("downloaded")
+
+        run(input_path, render_factor)
+        print("converted")
+
+        callback = send_file(output_path, mimetype='image/jpeg')
+    
+        return callback, 200
+
+    except DownloadPrecheckFailed as e:
+        return jsonify({'message': str(e)}), 400
+    except:
+        traceback.print_exc()
+        return jsonify({'message': 'inference error'}), 500
+
+    finally:
+        pass
+        clean_all([
+            input_path,
+            output_path
+            ])
+
+@app.route("/process-img-form", methods=["POST"])
+def processToForm():
+    input_path = generate_random_filename(upload_directory,"jpeg")
+    output_path = os.path.join(results_img_directory, os.path.basename(input_path))
+
+    # passed through form
+    image = request.files['image']
+
+    image.save(input_path)
+
+    run(input_path)
+
+    callback = send_file(output_path, mimetype='image/jpeg')
+    
+    return callback, 200
+
+
+def run(input_path, render_factor = 35):
+    try:
+        image_colorizer.plot_transformed_image(path=input_path, figsize=(20,20),
+        render_factor=render_factor, display_render_factor=True, compare=False)
+    except:
+        convertToJPG(input_path)
+        image_colorizer.plot_transformed_image(path=input_path, figsize=(20,20),
+        render_factor=render_factor, display_render_factor=True, compare=False)
+
+    return True
+    
+
+@app.route('/health')
+def health():
+    return "ok"
+
+@app.route('/')
+def main():
+    return app.send_static_file('index.html')
+    
+if __name__ == '__main__':
+    global upload_directory
+    global results_img_directory
+    global image_colorizer
+    #global video_colorizer
+
+    upload_directory = '/data/upload/'
+    #create_directory(upload_directory)
+
+    results_img_directory = '/data/result_images/'
+    #create_directory(results_img_directory)
+
+    model_directory = '/data/models/'
+    #create_directory(model_directory)
+
+    #artistic_model_url = 'https://www.dropbox.com/s/zkehq1uwahhbc2o/ColorizeArtistic_gen.pth?dl=0'
+    #get_model_bin(artistic_model_url, os.path.join(model_directory, 'ColorizeArtistic_gen.pth'))
+
+    image_colorizer = get_image_colorizer(artistic=True)
+
+
+    print('ready for')
+    app.run(host='0.0.0.0', port=80, threaded=False)
+
